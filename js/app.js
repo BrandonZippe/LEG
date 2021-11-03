07123b6bf64682271e77bcf8bcab95676684f686
Brandon Zippe <btzippe@gmail.com>
2021-11-03 09:45:16 -0500

    removing react from project

--- js/app.js
+++ js/app.js
@@ -1164,28 +1164,3 @@ $('.sortBtn').click(function() {
 
     dataHandler.sortTeams(method);
 });
-
-
-// react
-'use strict';
-
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
